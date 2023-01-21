a0d98039c6e01373a73ea442e5058c0471ebabcb
v-Alqorith <vahapcandongel@hotmail.com>
2023-01-21 21:35:18 +0000

    commit

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -61,7 +61,7 @@ export default function Dashboard() {
         ) {
           return (
             <div className="w-full h-full flex justify-center items-center text-xl">
-              No option selected
+              No option selected.
             </div>
           );
         }
