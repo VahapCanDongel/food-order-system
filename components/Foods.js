ecdf1f23f3adcca9a5f768802c6353db2dfa5f10
v-Alqorith <vahapcandongel@hotmail.com>
2023-01-22 21:39:44 +0000

    data can now be added correctly

--- components/Foods.js
+++ components/Foods.js
@@ -1,8 +1,20 @@
 import { auth, db } from "@/utils/firebase";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  onSnapshot,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+  getDoc,
+} from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { async } from "@firebase/util";
 
 export default function Foods() {
   const [itemName, setItemName] = useState({ description: "" });
@@ -15,25 +27,32 @@ export default function Foods() {
 
   const submitFoodForm = async (e) => {
     e.preventDefault();
-    const collectionRef = collection(db, "users");
+    const documentRef = doc(db, "users", user.uid);
 
-    await addDoc(collectionRef, {
+    await setDoc(documentRef, {
       user: user.uid,
       user_name: user.displayName,
       user_email: user.email,
-      food: [
-        {
-          name: itemName,
-          price: itemPrice,
-          parent_category: parentCategory,
-        },
-      ],
+      food: [],
+    });
+  };
+
+  const updateFoodArray = async (e) => {
+    e.preventDefault();
+    const documentRef = doc(db, "users", user.uid);
+    await updateDoc(documentRef, {
+      food: arrayUnion({
+        name: itemName,
+        price: itemPrice,
+        parent_category: parentCategory,
+      }),
     });
   };
 
   const getData = async () => {
     const getDataCollectionRef = collection(db, "users");
     const q = query(getDataCollectionRef);
+
     const unsubscribe = onSnapshot(q, (snapshot) => {
       setAllFoods(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     });
@@ -85,7 +104,10 @@ export default function Foods() {
           </select>
         </div>
 
-        <button onClick={submitFoodForm} className="bg-teal-700 text-white p-3">
+        <button
+          onClick={(submitFoodForm, updateFoodArray)}
+          className="bg-teal-700 text-white p-3"
+        >
           Add Item
         </button>
       </form>
@@ -98,20 +120,22 @@ export default function Foods() {
           <th className="border-[2px] border-black">Extras</th>
         </tr>
 
-        {allFoods.map((food) => (
-          <tr>
-            <td className="border-[2px] border-black">
-              {food.food[0].name.description}
-            </td>
-            <td className="border-[2px] border-black">
-              {food.food[0].price.description}
-            </td>
-            <td className="border-[2px] border-black">
-              {food.food[0].parent_category.description}
-            </td>
-            <td className="border-[2px] border-black"></td>
-          </tr>
-        ))}
+        {allFoods.map((food) =>
+          food.food.map((item) => (
+            <tr>
+              <td className="border-[2px] border-black">
+                {item.name.description}
+              </td>
+              <td className="border-[2px] border-black">
+                {item.price.description}
+              </td>
+              <td className="border-[2px] border-black">
+                {item.parent_category.description}
+              </td>
+              <td className="border-[2px] border-black"></td>
+            </tr>
+          ))
+        )}
       </table>
     </div>
   );
