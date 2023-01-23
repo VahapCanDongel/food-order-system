8d7c1c71c09e5e85594db4ee253bab18e1970829
v-Alqorith <vahapcandongel@hotmail.com>
2023-01-23 20:27:59 +0000

    UI has been changed however, I need to create sub collections for category and food

--- components/Foods.js
+++ components/Foods.js
@@ -24,10 +24,12 @@ export default function Foods() {
   const [allFoods, setAllFoods] = useState([]);
   const [itemID, setItemID] = useState(0);
 
+  const [categoryName, setCategoryName] = useState({ description: "" });
+
   const [user, loading] = useAuthState(auth);
   const route = useRouter();
 
-  const submitFoodForm = async (e) => {
+  const generalFormSubmit = async (e) => {
     e.preventDefault();
     const documentRef = doc(db, "users", user.uid);
 
@@ -35,12 +37,9 @@ export default function Foods() {
       user: user.uid,
       user_name: user.displayName,
       user_email: user.email,
-      food: [
+      category: [
         {
-          id: uuidv4(),
-          name: itemName,
-          price: itemPrice,
-          parent_category: parentCategory,
+          food: [],
         },
       ],
     });
@@ -55,16 +54,35 @@ export default function Foods() {
       submitFoodForm(e);
     } else {
       await updateDoc(documentRef, {
-        food: arrayUnion({
-          id: uuidv4(),
-          name: itemName,
-          price: itemPrice,
-          parent_category: parentCategory,
+        categor: arrayUnion({
+          food: arrayUnion({
+            id: uuidv4(),
+            name: itemName,
+            price: itemPrice,
+            parent_category: parentCategory,
+          }),
         }),
       });
     }
   };
 
+  const updateCategoryArray = async (e) => {
+    e.preventDefault();
+
+    const docCollection = collection(db, "users");
+    const documentRef = doc(db, "users", user.uid);
+    if ((await (await getDocs(docCollection)).empty) == true) {
+      generalFormSubmit(e);
+    } else {
+      await updateDoc(documentRef, {
+        category: arrayUnion({
+          id: uuidv4(),
+          name: categoryName,
+          food: [],
+        }),
+      });
+    }
+  };
   const checkingItemID = async (itemID) => {
     setItemID(itemID);
     const documentRef = doc(db, "users", user.uid);
@@ -90,78 +108,166 @@ export default function Foods() {
 
   return (
     <div className="w-full h-screen flex justify-evenly flex-col items-center">
-      <form className="flex justify-evenly items-center gap-10">
-        <div className="flex flex-col gap-2 justify-center">
-          <label className="text-xl">Name: </label>
-          <input
-            onChange={(e) =>
-              setItemName({ ...itemName, description: e.target.value })
-            }
-            type="text"
-            className="border-[2px] border-teal-400 p-2 w-[240px] rounded-md"
-          />
-        </div>
+      {/* Add Food Form */}
+      <div className="flex  gap-5">
+        <form className="flex flex-col justify-evenly items-center gap-10 w-[400px] p-4 rounded-md shadow-xl border-[1px] border-teal-400">
+          <div className="flex flex-col gap-2 justify-center">
+            <label className="text-xl"> Category Name: </label>
+            <input
+              onChange={(e) => {
+                setCategoryName({
+                  ...categoryName,
+                  description: e.target.value,
+                });
+              }}
+              type="text"
+              className="border-[2px] border-teal-400 p-2 w-[240px] rounded-md"
+            />
+          </div>
 
-        <div className="flex flex-col gap-2 justify-center">
-          <label className="text-xl">Price: </label>
-          <input
-            onChange={(e) =>
-              setItemPrice({ ...itemPrice, description: e.target.value })
-            }
-            type="text"
-            className="border-[2px] border-teal-400 p-2 w-[240px] rounded-md"
-          />
-        </div>
-
-        <div className="flex flex-col gap-2 justify-center">
-          <label className="text-xl">Parent Category: </label>
-
-          <select
-            onChange={(e) =>
-              setParentCategory({
-                ...parentCategory,
-                description: e.target.value,
-              })
-            }
-            className="border-[2px] border-teal-400 p-2 h-[44px] w-[240px] rounded-md"
+          <button
+            className="bg-teal-500 text-white p-3 rounded-md"
+            onClick={updateCategoryArray}
           >
-            <option>Test</option>
-          </select>
-        </div>
+            Add Category
+          </button>
+        </form>
 
-        <button
-          onClick={(submitFoodForm, updateFoodArray)}
-          className="bg-teal-700 text-white p-3"
-        >
-          Add Item
-        </button>
-      </form>
+        <form className="flex flex-col justify-evenly items-center gap-10 w-[400px] p-4 rounded-md shadow-xl border-[1px] border-teal-400">
+          <div className="flex flex-col gap-2 justify-center">
+            <label className="text-xl">Food Name: </label>
+            <input
+              onChange={(e) =>
+                setItemName({ ...itemName, description: e.target.value })
+              }
+              type="text"
+              className="border-[2px] border-teal-400 p-2 w-[240px] rounded-md"
+            />
+          </div>
 
-      <table className="border-[2px] border-black w-[800px] text-center">
-        <tr className="border-[2px] border-black">
-          <th className="border-[2px] border-black">Name</th>
-          <th className="border-[2px] border-black">Price</th>
-          <th className="border-[2px] border-black">Parent Category</th>
-          <th className="border-[2px] border-black">Extras</th>
-          <th className="border-[2px] border-black">Operations</th>
+          <div className="flex flex-col gap-2 justify-center">
+            <label className="text-xl">Food Price: </label>
+            <input
+              onChange={(e) =>
+                setItemPrice({ ...itemPrice, description: e.target.value })
+              }
+              type="text"
+              className="border-[2px] border-teal-400 p-2 w-[240px] rounded-md"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2 justify-center">
+            <label className="text-xl">Parent Category: </label>
+
+            <select
+              onChange={(e) =>
+                setParentCategory({
+                  ...parentCategory,
+                  description: e.target.value,
+                })
+              }
+              className="border-[2px] border-teal-400 p-2 h-[44px] w-[240px] rounded-md"
+            >
+              <option>Test</option>
+            </select>
+          </div>
+
+          <button
+            onClick={updateFoodArray}
+            className="bg-teal-500 text-white p-3 rounded-md"
+          >
+            Add Food
+          </button>
+        </form>
+
+        <form className="flex flex-col justify-evenly items-center gap-10 w-[400px] p-4 rounded-md shadow-xl border-[1px] border-teal-400">
+          <div className="flex flex-col gap-2 justify-center">
+            <label className="text-xl">Selected Food:</label>
+
+            <select className="border-[2px] border-teal-400 p-2 h-[44px] w-[240px] rounded-md">
+              {allFoods.map((item) => {
+                item.category.map((food) => {
+                  <option>food</option>;
+                });
+              })}
+            </select>
+          </div>
+
+          <div className="flex flex-col gap-2 justify-center">
+            <label className="text-xl">Extra Item Name: </label>
+            <input
+              type="text"
+              className="border-[2px] border-teal-400 p-2 w-[240px] rounded-md"
+            />
+
+            <button className="bg-teal-400 text-white p-3 w-[90px] rounded-md">
+              Add
+            </button>
+          </div>
+
+          <div className="w-[300px] bg-teal-400 min-h-[100px] rounded-md flex justify-center p-1">
+            <div className="text-xs bg-white w-[80px] flex justify-center items-center rounded-sm h-[20px]">
+              <button>
+                <svg
+                  width="46"
+                  height="46"
+                  fill="none"
+                  stroke="#d95454"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[15px] text-red-900"
+                >
+                  <path d="M6.343 17.657 17.657 6.344m-11.314 0 11.314 11.313L6.343 6.344Z"></path>
+                </svg>
+              </button>
+              Tomato
+            </div>
+          </div>
+
+          <button className="bg-teal-500 text-white p-3 rounded-md">
+            Complete
+          </button>
+        </form>
+      </div>
+
+      {/* <table className="border-[1px] border-teal-500 w-[800px] text-center">
+        <tr className="border-[2px] border-teal-500">
+          <th className="border-[2px] border-b-black border-t-black border-l-black bg-teal-500 text-white">
+            Name
+          </th>
+          <th className="border-[2px] border-b-black border-t-black bg-teal-500 text-white">
+            Price
+          </th>
+          <th className="border-[2px] border-b-black border-t-black bg-teal-500 text-white">
+            Parent Category
+          </th>
+          <th className="border-[2px] border-b-black border-t-black bg-teal-500 text-white">
+            Extras
+          </th>
+          <th className="border-[2px] border-b-black border-t-black border-r-black bg-teal-500 text-white">
+            Operations
+          </th>
         </tr>
 
         {allFoods.map((food) =>
           food.food.map((item) => (
             <tr>
-              <td className="border-[2px] border-black">
+              <td className="border-[2px] border-teal-500">
                 {item.name.description}
               </td>
-              <td className="border-[2px] border-black">
+              <td className="border-[2px] border-teal-500">
                 {item.price.description}
               </td>
-              <td className="border-[2px] border-black">
+              <td className="border-[2px] border-teal-500">
                 {item.parent_category.description}
               </td>
-              <td className="border-[2px] border-black"></td>
-              <td className="border-[2px] border-black">
+              <td className="border-[2px] border-teal-500"></td>
+              <td className="border-[2px] border-teal-500">
                 <button
-                  className="bg-red-400 text-white text-center rounded-md ml-4 h-[25px] w-[80px]"
+                  className="bg-red-300 text-white text-center rounded-md ml-4 h-[25px] w-[80px] hover:bg-red-400"
                   onClick={() => checkingItemID(item)}
                 >
                   Delete
@@ -170,7 +276,7 @@ export default function Foods() {
             </tr>
           ))
         )}
-      </table>
+      </table> */}
     </div>
   );
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,5 @@
-import Categories from "@/components/Categories";
-import Extras from "@/components/Extras";
 import Foods from "@/components/Foods";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/utils/firebase";
 import { useRouter } from "next/router";
@@ -20,68 +18,9 @@ export default function Dashboard() {
     getData();
   }, [user, loading]);
 
-  const [category_visbility, setCategoryVisibility] = useState(false);
-  const [food_visbility, setFoodVisibility] = useState(false);
-  const [extra_visbility, setExtraVisibility] = useState(false);
-
-  const categoriesClick = () => {
-    setCategoryVisibility(true);
-    setExtraVisibility(false);
-    setFoodVisibility(false);
-    console.log(category_visbility, food_visbility, extra_visbility);
-  };
-  const foodsClick = () => {
-    setCategoryVisibility(false);
-    setExtraVisibility(false);
-    setFoodVisibility(true);
-  };
-  const extrasClick = () => {
-    setCategoryVisibility(false);
-    setExtraVisibility(true);
-    setFoodVisibility(false);
-  };
-
   return (
     <div className="w-full h-screen flex">
-      {/* Navigation */}
-      <div className="flex flex-col w-[10%] h-full  border-r-[2px] border-teal-400 justify-evenly items-center">
-        <button
-          className="text-xl rounded-md text-white p-2 w-[150px] text-center bg-teal-400"
-          onClick={categoriesClick}
-        >
-          Categories
-        </button>
-        <button
-          className="text-xl rounded-md text-white p-2 w-[150px] text-center bg-teal-400"
-          onClick={foodsClick}
-        >
-          Foods
-        </button>
-        <button
-          className="text-xl rounded-md text-white p-2 w-[150px] text-center bg-teal-400"
-          onClick={extrasClick}
-        >
-          Extras
-        </button>
-      </div>
-
-      {category_visbility && <Categories />}
-      {food_visbility && <Foods />}
-      {extra_visbility && <Extras />}
-
-      {(() => {
-        if (
-          category_visbility == false &&
-          food_visbility == false &&
-          extra_visbility == false
-        ) {
-          return (
-            <div className="w-full h-full flex justify-center items-center text-xl">
-              No option selected.
-            </div>
-          );
-        }
-      })()}
+      <Foods />
     </div>
   );
 }
