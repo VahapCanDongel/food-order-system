590654abb905d640c6578420b7af49b9a19ae00d
VahapCanDongel <vahapcandongel@hotmail.com>
2023-01-27 15:22:15 +0000

    commit

--- components/Categories.js
+++ components/Categories.js
@@ -1,6 +1,6 @@
 export default function Categories() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center gap-4">
           <div className="bg-white w-[250px] h-[100px] rounded-md shadow-lg flex justify-center items-center hover:cursor-pointer">
               <div className="text-black flex justify-center items-center text-[24px]">
                   Kebab
